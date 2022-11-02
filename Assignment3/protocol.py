b2a1cdcebe972c65881e5dfdf283625c0b1d2c70
Ying Qi Wen <59152423+wenyingqi@users.noreply.github.com>
2022-11-02 16:18:19 -0700

    Update protocol.py

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -65,6 +65,36 @@ class Protocol:
         return plain_text + hash_ciphertext
 
 
+    # Encrypting messages
+    # Encrypted message is of the form:
+    #       E("Alice/Bob", msg, msg#, KS), MAC
+    # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
+    # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
+    def EncryptAndProtectMessage(self, plain_text):
+        cipher_text = plain_text
+        # Only for encrypting messages (not protocol)
+        # Remember to set self.numMsg (and update it for each message)
+        # Uses a hash (SHA 256)
+
+        message_in_binary = plain_text.encode('utf-8')
+
+        self.keySession = b'Sixteen byte key'
+        self.RA = b'ra'
+        key = self.keySession
+
+        # I assume that the nonce will be wither one of RA or RB
+        # But I am not sure whether both instances of protocol have both RA and RB,
+        # To make sure, when establishing the protocol, we did record both RA and RB, right?
+        cipher = AES.new(key, AES.MODE_EAX, nonce=self.RA)
+
+        ciphertext, tag = cipher.encrypt_and_digest(message_in_binary) #ciphertext is binary
+        string_ciphertext = b64encode(ciphertext).decode('utf-8')      #string_ciphertext is string
+        print('string_ciphertext:' + string_ciphertext)
+        hash_ciphertext = hashlib.sha256(ciphertext).hexdigest()       #hashed the binary of ciphertext (in binary)
+
+        self.numMsg = self.numMsg + 1
+        return 'Message' + string_ciphertext + hash_ciphertext
+
     # Decrypting and verifying messages
     # Encrypted message is of the form:
     #       E("Alice/Bob", msg, msg#, KS), MAC
@@ -77,17 +107,19 @@ class Protocol:
         # Uses a hash
 
         total_length = len(cipher_text)
-        hash = cipher_text[total_length-64: total_length]
-        ciphertext_without_hash = cipher_text[0: total_length-64]
+        hash = cipher_text[total_length - 64: total_length]
+        ciphertext_without_hash = cipher_text[7: total_length - 64]
+        print('here')
+
+        cipher_binary = b64decode(ciphertext_without_hash)
 
         cipher = AES.new(self.keySession, AES.MODE_EAX, nonce=self.RA)
-        plaintext = cipher.decrypt(ciphertext_without_hash)
+        plaintext = cipher.decrypt(cipher_binary)
 
-        rehash = hashlib.sha256(plaintext).hexdigest()
-        
-        if hash.equals(rehash):
-            plaintext_without_message = plaintext[7:]
+        rehash = hashlib.sha256(cipher_binary).hexdigest()
+
+        if hash.decode('utf-8') == rehash:
             self.numMsg = self.numMsg + 1
-            return plaintext_without_message
+            return plaintext
         else:
-            return 'ERROR 0465: Integrity failed'
+            return plaintext
