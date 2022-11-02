f43c9b235d0daa4497c84aa20f98df923283c0b1
Ying Qi Wen <59152423+wenyingqi@users.noreply.github.com>
2022-11-02 16:25:09 -0700

    Update protocol.py

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -162,29 +162,63 @@ class Protocol:
 
 
     # Message functions
-
+    
     # Encrypting messages
     # Encrypted message is of the form:
-    #       "Message", E("A/B", msg, msg#, KS), H("A/B", msg, msg#)
+    #       E("Alice/Bob", msg, msg#, KS), MAC
     # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def EncryptAndProtectMessage(self, plain_text):
         cipher_text = plain_text
+        # Only for encrypting messages (not protocol)
         # Remember to set self.numMsg (and update it for each message)
-        # Encrypt message with KS
-        # Calculate the MAC
-        return "Message" + cipher_text
+        # Uses a hash (SHA 256)
 
+        message_in_binary = plain_text.encode('utf-8')
+
+        #self.keySession = b'Sixteen byte key' here only for testing purposes
+        #self.RA = b'ra'
+        key = self.keySession
+
+        # I assume that the nonce will be wither one of RA or RB
+        # But I am not sure whether both instances of protocol have both RA and RB,
+        # To make sure, when establishing the protocol, we did record both RA and RB, right?
+        cipher = AES.new(key, AES.MODE_EAX, nonce=self.RA)
+
+        ciphertext, tag = cipher.encrypt_and_digest(message_in_binary) #ciphertext is binary
+        string_ciphertext = b64encode(ciphertext).decode('utf-8')      #string_ciphertext is string
+
+        hash_ciphertext = hashlib.sha256(ciphertext).hexdigest()       #hashed the binary of ciphertext (in binary)
+
+        self.numMsg = self.numMsg + 1
+        return 'Message' + string_ciphertext + hash_ciphertext
 
     # Decrypting and verifying messages
     # Encrypted message is of the form:
-    #       "Message", E("A/B", msg, msg#, KS), H("A/B", msg, msg#)
+    #       E("Alice/Bob", msg, msg#, KS), MAC
     # TODO: IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def DecryptAndVerifyMessage(self, cipher_text):
         plain_text = cipher_text
-        # Remove "Message" from the front
-        # Split into E() and H(MAC)
-        # Verify MAC
-        # Decrypt E()
-        return plain_text
+        # Only for decrypting and verifying messages
+        # Remember to set self.numMsg (and update it for each message)
+        # Uses a hash
+
+        total_length = len(cipher_text)
+        hash = cipher_text[total_length - 64: total_length]
+        ciphertext_without_hash = cipher_text[7: total_length - 64]
+
+        cipher_binary = b64decode(ciphertext_without_hash)
+
+        cipher = AES.new(self.keySession, AES.MODE_EAX, nonce=self.RA)
+        plaintext = cipher.decrypt(cipher_binary)
+
+        rehash = hashlib.sha256(cipher_binary).hexdigest()
+
+        if hash.decode('utf-8') == rehash:
+
+            self.numMsg = self.numMsg + 1
+            return plaintext
+        else:
+            raise Exception("Integrity compromised")
+
